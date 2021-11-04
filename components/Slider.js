b29b4c34883f6fd00dc079c44b89adf33165e49d
Darlley <systembrtt@gmail.com>
2021-11-03 22:07:00 -0400

    imagens instagram slider

--- components/Slider.js
+++ components/Slider.js
@@ -1,44 +1,64 @@
-import {useRef} from 'react'
-import Image from 'next/image';
+import { useRef } from 'react'
+import Image from 'next/image'
 
-import styles from '../styles/Slider.module.scss';
+import styles from '../styles/Slider.module.scss'
 
-import perfilImg1 from '../public/elipse_card.png'; 
-import slideSetaEsquerda from '../public/slide-seta-esquerda.png'; 
-import slideSetaDireita from '../public/slide-seta-direita.png'; 
+import instagramAlecacaushow from '../public/instagram-alecacaushow.png'
+import instagramToguro from '../public/instagram-toguro.png'
+import instagramBunitotrader from '../public/instagram-bunitotrader.png'
+import instagramDeiveleonardo from '../public/instagram-deiveleonardo.png'
+import instagramThiagonigro from '../public/instagram-thiagonigro.png'
+import instagramDarlley from '../public/instagram-darlley.jpg'
+
+import slideSetaEsquerda from '../public/slide-seta-esquerda.png'
+import slideSetaDireita from '../public/slide-seta-direita.png'
 
 export default function Slider() {
   const slider = useRef(null)
 
-  const handleLeftClick = (e) => {
-    e.preventDefault();
+  const handleLeftClick = e => {
+    e.preventDefault()
 
-    slider.current.scrollLeft -= slider.current.offsetWidth;
+    slider.current.scrollLeft -= slider.current.offsetWidth
   }
-  const handleRightClick = (e) => {
-    e.preventDefault();
+  const handleRightClick = e => {
+    e.preventDefault()
 
-    slider.current.scrollLeft += slider.current.offsetWidth;
+    slider.current.scrollLeft += slider.current.offsetWidth
   }
   return (
     <div className={styles.slider}>
       <div className={styles.buttonsContainer}>
-        <button className={styles.buttonsContainerButton} onClick={handleLeftClick} onTouchStart={handleLeftClick}>
-          <Image 
-            src={slideSetaEsquerda}
-            width={60}
-            height={60}
-          />
+        <button
+          className={styles.buttonsContainerButton}
+          onClick={handleLeftClick}
+          onTouchStart={handleLeftClick}
+        >
+          <Image src={slideSetaEsquerda} width={60} height={60} />
         </button>
-        <button className={styles.buttonsContainerButton} onClick={handleRightClick} onTouchStart={handleRightClick}>
-          <Image 
-              src={slideSetaDireita}
-              width={60}
-              height={60}
-            />
+        <button
+          className={styles.buttonsContainerButton}
+          onClick={handleRightClick}
+          onTouchStart={handleRightClick}
+        >
+          <Image src={slideSetaDireita} width={60} height={60} />
         </button>
       </div>
       <div className={styles.slideCards} ref={slider}>
+      <div className={styles.slideCard}>
+          <a href="#">
+            <div className={styles.slideCardIconOpen}>
+              <i className="bi bi-arrow-up-right-square"></i>
+            </div>
+            <div className={styles.slideCardContent}>
+              <div className={styles.slideCardContentImage}>
+                <Image src={instagramDarlley} width={132} height={132} />
+              </div>
+              <h3>@darlleybbf</h3>
+              <p>671 seguidores no Instagram</p>
+            </div>
+          </a>
+        </div>
 
         <div className={styles.slideCard}>
           <a href="#">
@@ -47,11 +67,7 @@ export default function Slider() {
             </div>
             <div className={styles.slideCardContent}>
               <div className={styles.slideCardContentImage}>
-                <Image 
-                  src={perfilImg1}
-                  width={132}
-                  height={132}
-                />
+                <Image src={instagramAlecacaushow} width={132} height={132} />
               </div>
               <h3>@alecacaushow</h3>
               <p>1,3Mi seguidores no Istagram</p>
@@ -66,14 +82,10 @@ export default function Slider() {
             </div>
             <div className={styles.slideCardContent}>
               <div className={styles.slideCardContentImage}>
-                <Image 
-                  src={perfilImg1}
-                  width={132}
-                  height={132}
-                />
+                <Image src={instagramToguro} width={132} height={132} />
               </div>
-              <h3>@alecacaushow</h3>
-              <p>1,3Mi seguidores no Istagram</p>
+              <h3>@toguro</h3>
+              <p>4,4Mi seguidores no Instagram</p>
             </div>
           </a>
         </div>
@@ -85,14 +97,10 @@ export default function Slider() {
             </div>
             <div className={styles.slideCardContent}>
               <div className={styles.slideCardContentImage}>
-                <Image 
-                  src={perfilImg1}
-                  width={132}
-                  height={132}
-                />
+                <Image src={instagramBunitotrader} width={132} height={132} />
               </div>
-              <h3>@alecacaushow</h3>
-              <p>1,3Mi seguidores no Istagram</p>
+              <h3>@bunitotrader</h3>
+              <p>275K seguidores no Instagram</p>
             </div>
           </a>
         </div>
@@ -104,14 +112,10 @@ export default function Slider() {
             </div>
             <div className={styles.slideCardContent}>
               <div className={styles.slideCardContentImage}>
-                <Image 
-                  src={perfilImg1}
-                  width={132}
-                  height={132}
-                />
+                <Image src={instagramDeiveleonardo} width={132} height={132} />
               </div>
-              <h3>@alecacaushow</h3>
-              <p>1,3Mi seguidores no Istagram</p>
+              <h3>@deiveleonardo</h3>
+              <p>9,7Mi seguidores no Instagram</p>
             </div>
           </a>
         </div>
@@ -123,56 +127,15 @@ export default function Slider() {
             </div>
             <div className={styles.slideCardContent}>
               <div className={styles.slideCardContentImage}>
-                <Image 
-                  src={perfilImg1}
-                  width={132}
-                  height={132}
-                />
+                <Image src={instagramThiagonigro} width={132} height={132} />
               </div>
-              <h3>@alecacaushow</h3>
-              <p>1,3Mi seguidores no Istagram</p>
+              <h3>@thiago.nigro</h3>
+              <p>5,5Mi seguidores no Instagram</p>
             </div>
           </a>
         </div>
 
-        <div className={styles.slideCard}>
-          <a href="#">
-            <div className={styles.slideCardIconOpen}>
-              <i className="bi bi-arrow-up-right-square"></i>
-            </div>
-            <div className={styles.slideCardContent}>
-              <div className={styles.slideCardContentImage}>
-                <Image 
-                  src={perfilImg1}
-                  width={132}
-                  height={132}
-                />
-              </div>
-              <h3>@alecacaushow</h3>
-              <p>1,3Mi seguidores no Istagram</p>
-            </div>
-          </a>
-        </div>
-
-        <div className={styles.slideCard}>
-          <a href="#">
-            <div className={styles.slideCardIconOpen}>
-              <i className="bi bi-arrow-up-right-square"></i>
-            </div>
-            <div className={styles.slideCardContent}>
-              <div className={styles.slideCardContentImage}>
-                <Image 
-                  src={perfilImg1}
-                  width={132}
-                  height={132}
-                />
-              </div>
-              <h3>@alecacaushow</h3>
-              <p>1,3Mi seguidores no Istagram</p>
-            </div>
-          </a>
-        </div>
-
+        <div className={styles.slideCard}></div>
       </div>
     </div>
   )
