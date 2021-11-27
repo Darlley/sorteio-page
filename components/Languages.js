d0e1dda5cc4e3bd1e7b4e7be66fbd59a6e61e9e3
Darlley <systembrtt@gmail.com>
2021-11-27 16:17:13 -0400

    att

--- components/Languages.js
+++ components/Languages.js
@@ -1,11 +1,15 @@
+import { useState } from 'react'
+
 import Image from 'next/image'
 import flagbr from '../public/flagbr.svg'
 
 import styles from '../styles/Languages.module.scss'
 
 export const Languages = () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className={`ms-lg-5 ${styles.languages}`}>
+    <div className={`ms-lg-5 ${styles.languages}`} onClick={() => setOpen(!open)}>
       <div className={styles.languagesContainer}>
         <div>
           <Image src={flagbr} width={25} heigh={25} />
@@ -18,7 +22,7 @@ export const Languages = () => {
         </div>
       </div>
 
-      <div className={styles.languagesContainer}>
+      <div className={`${styles.languagesContainer} ${open ? styles.open : ''}`}>
         <ul>
           <li>
             <Image src={flagbr} width={20} heigh={20} />
